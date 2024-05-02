3a6cd2f5cc85630dc328e4452312e7198d3d0590
Hai Feng Kao <haifeng@cocoaspice.in>
2024-05-02 17:34:57 +0800

    add tab view for detail info

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter_resizable_container/flutter_resizable_container.dart';
 import 'package:modkeeper/module_selection_screen.dart';
 import 'package:modkeeper/utilities.dart';
 import 'dart:io';
@@ -8,6 +9,28 @@ import 'module_item.dart';
 import 'package:path/path.dart' as path;
 import 'configuration_view.dart';
 import 'package:path_provider/path_provider.dart';
+
+class ModTab {
+  final String title;
+  final Widget Function() buildView;
+
+  ModTab({required this.title, required this.buildView});
+}
+
+List<ModTab> _modTabs = [
+  ModTab(
+    title: 'Installation Logs',
+    buildView: () => Center(child: Text('Installation Logs')),
+  ),
+  ModTab(
+    title: 'Mod Info',
+    buildView: () => Center(child: Text('Mod Info')),
+  ),
+  ModTab(
+    title: 'Mod Conflicts',
+    buildView: () => Center(child: Text('Mod Conflicts')),
+  ),
+];
 
 void main() {
   runApp(const MyApp());
@@ -19,12 +42,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ModKeeper',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage()
+        title: 'ModKeeper',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage()
     );
   }
 }
@@ -60,6 +83,22 @@ class MyHomePageState extends State<MyHomePage> {
     }
   }
 
+  Widget createTabView() {
+    return DefaultTabController(
+      length: _modTabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            tabs: _modTabs.map((tab) => Tab(text: tab.title)).toList(),
+          ),
+        ),
+        body: TabBarView(
+          children: _modTabs.map((tab) => tab.buildView()).toList(),
+        ),
+      ),
+    );
+  }
+
   Widget createConfigurationView() {
     return ConfigurationView(
       onSaveConfiguration: (settings) {
@@ -71,28 +110,46 @@ class MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  Widget resizeableContainer() {
+    return ResizableContainer(
+      direction: Axis.horizontal,
+      dividerWidth: 10,
+      dividerColor: Colors.grey[300],
+      children: [
+        ResizableChildData(
+          startingRatio: 0.3,
+          minSize: 200,
+          child: futureLoadingWidget,
+        ),
+        ResizableChildData(
+          startingRatio: 0.7,
+          child: createTabView()
+        ),
+      ],
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ModKeeper'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.settings, size: 40),
-            onPressed: () {
-              setState(() {
-                showConfigurationView = !showConfigurationView;
-              });
-            },
-          ),
-        ],
-      ),
-      body: Stack(
-        children: [
-          futureLoadingWidget,
-          if (showConfigurationView) createConfigurationView(),
-        ],
-      ),
+        appBar: AppBar(
+          title: const Text('ModKeeper'),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.settings, size: 40),
+              onPressed: () {
+                setState(() {
+                  showConfigurationView = !showConfigurationView;
+                });
+              },
+            ),
+          ],
+        ),
+        body: Stack(
+          children: [
+              resizeableContainer(),
+              if (showConfigurationView) createConfigurationView(),
+          ],
+        )
     );
   }
 }
